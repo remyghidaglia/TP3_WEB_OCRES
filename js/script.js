c52abeca66c16d3f8c6bceb8c950954b70b68410
remyghidaglia <remycaousou@gmail.com>
2020-10-13 10:59:40 +0200

    q3

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // Fonction appelée lors du click du bouton
 function start() {
   // Création de l'objet apiWeather
-  const apiWeather = new API_WEATHER();
+  const apiWeather = new API_WEATHER(document.getElementById('city-input').value);
   // Appel de la fonction fetchTodayForecast
 
   apiWeather
